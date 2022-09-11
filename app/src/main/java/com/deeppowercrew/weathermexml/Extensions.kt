06499bbc0e1dd99eb00e2754a0c009cea14fff27
DT5Gen <marinovod@gmail.com>
2022-09-12 00:43:22 +0300

    add fun checkPermissions

---
+++ app/src/main/java/com/deeppowercrew/weathermexml/Extensions.kt
@@ -0,0 +1,14 @@
+package com.deeppowercrew.weathermexml
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+
+fun Fragment.isPermissionsGranted(permissions: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        activity as AppCompatActivity, permissions
+    ) == PackageManager.PERMISSION_GRANTED
+
+}
--- app/src/main/java/com/deeppowercrew/weathermexml/ui/MainFragment.kt
+++ app/src/main/java/com/deeppowercrew/weathermexml/ui/MainFragment.kt
@@ -1,14 +1,20 @@
 package com.deeppowercrew.weathermexml.ui
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.deeppowercrew.weathermexml.databinding.FragmentMainBinding
+import com.deeppowercrew.weathermexml.isPermissionsGranted
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
 
     override fun onCreateView(
@@ -18,6 +24,31 @@ class MainFragment : Fragment() {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermissions()
+    }
+
+
+    private fun permissionListener() {
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                Toast.makeText(activity, "Permissions is $it", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+    private fun checkPermissions(){
+if (!isPermissionsGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+    permissionListener()
+    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+}
+
+
+    }
+
 
     companion object {
 
