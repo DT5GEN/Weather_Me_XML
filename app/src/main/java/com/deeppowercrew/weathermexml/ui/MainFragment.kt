16a4863b5390e0eb003768fc09023ed9c8f9f216
DT5Gen <marinovod@gmail.com>
2022-09-12 03:14:48 +0300

    add tabLayouts

--- app/src/main/java/com/deeppowercrew/weathermexml/ui/MainFragment.kt
+++ app/src/main/java/com/deeppowercrew/weathermexml/ui/MainFragment.kt
@@ -10,8 +10,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import com.deeppowercrew.weathermexml.R
 import com.deeppowercrew.weathermexml.adapters.ViewPagerAdapter
 import com.deeppowercrew.weathermexml.databinding.FragmentMainBinding
 import com.deeppowercrew.weathermexml.isPermissionsGranted
@@ -33,7 +31,7 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -45,11 +43,11 @@ class MainFragment : Fragment() {
         init()
     }
 
-    private fun init() = with (binding){
+    private fun init() = with(binding) {
         val adapter = ViewPagerAdapter(activity as AppCompatActivity, fragmentList)
         viewPager.adapter = adapter
-        TabLayoutMediator(tabLayout, viewPager){
-tab ,pos -> tab.text = tabsList[pos]
+        TabLayoutMediator(tabLayout, viewPager) { tab, pos ->
+            tab.text = tabsList[pos]
         }.attach()
     }
 
@@ -62,18 +60,17 @@ tab ,pos -> tab.text = tabsList[pos]
     }
 
 
-    private fun checkPermissions(){
-if (!isPermissionsGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
-    permissionListener()
-    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-}
+    private fun checkPermissions() {
+        if (!isPermissionsGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
 
 
     }
 
 
     companion object {
-
 
         @JvmStatic
         fun newInstance() = MainFragment()
